f0b74945824df2dc3516f0ce77caea7b9a9f7f89
Sachin Verma <vsachin094@gmail.com>
2023-12-19 12:46:19 +0530

    18/12/2023

--- acl_parse.py
+++ acl_parse.py
@@ -1,13 +1,14 @@
 import re
 
 def parse_acl_config(acl_config, acl_names=None):
-    acl_pattern = re.compile(r"ip access-list (?:standard|extended) (\S+)\n((?:\s+ \d+ )?(?: .*\n)+)")
+    acl_pattern = re.compile(r"ip access-list (?:standard|extended) (\S+)\s*\n((?:\s+ \d+ )?(?: .*\n)+)") 
 
     acls = {}
     for acl_match in acl_pattern.finditer(acl_config):
         acl_name = acl_match.group(1)
         if acl_names is None or acl_name in acl_names:
             aces = acl_match.group(2).strip().split("\n")
+            print(aces)
             aces = [re.sub(r"^\s+\d+\s+", "", ace) for ace in aces]
             acls[acl_name] = aces
     if acl_names is not None:
@@ -19,7 +20,7 @@ def parse_acl_config(acl_config, acl_names=None):
 
 
 acl_config = """
-ip access-list extended ACL_1
+ip access-list extended ACL_1 
   10 permit tcp 192.168.1.0 0.0.0.255 10.0.0.0 0.0.0.255 eq 80
   20 permit udp 192.168.1.0 0.0.0.255 10.0.0.0 0.0.0.255 eq 53
   30 deny ip any any
