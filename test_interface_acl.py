05bf5c6068acc14978a0406a8cb7d1a3eaf27848
Sachin Verma <vsachin094@gmail.com>
2023-12-19 19:23:28 +0530

    test

--- test_interface_acl.py
+++ test_interface_acl.py
@@ -1,52 +1,39 @@
 import re
 
-def get_interface_acls(interface_config_output, interface_names, acl_names):
-    acl_information = {}
-    for interface_name in interface_names:
-        # Regular expression pattern to match ACLs applied to the interface
-        acl_pattern = rf"interface {interface_name}\s*([\s\S]*?)(?=^interface|\Z)"
-
+def get_interface_acls(interface_config_output, interface_name, acl_names):
+    info = {}
+    for interface_ in interface_name:
+    # Regular expression pattern to match ACLs applied to the interface
+        acl_pattern = rf"interface {interface_}\s*([\s\S]*?)(?=^interface|\Z)"
         # Search for the ACL pattern in the interface configuration output
         match = re.search(acl_pattern, interface_config_output, re.MULTILINE)
-
         if match:
             acl_config = match.group(1)
             all_acls = re.findall(r"ip access-group (\S+) (in|out)", acl_config)
-            configured_acls = [acl[0] for acl in all_acls]
-            
+            configured_acls = [acl[0] for acl in all_acls] 
             acl_status = {acl_name: acl_name in configured_acls for acl_name in acl_names}
             other_acls = [acl for acl in configured_acls if acl not in acl_names]
-            
-            acl_information[interface_name] = {
-                'acl_status': acl_status,
-                'other_acls': other_acls
-            }
-        else:
-            acl_information[interface_name] = {
-                'acl_status': {},
-                'other_acls': []
-            }
-
-    return acl_information
-
-# Example usage with provided interface config output for two interfaces
+            acl_status.update({"another_acl_cont": other_acls})
+            info.update({interface_:acl_status})
+    return info
+# Example usage with provided interface config output
 interface_config_output = """
 interface GigabitEthernet1/1/1
  description Some description
  ip address 192.168.1.1 255.255.255.0
  ip access-group test_acl in
  ip access-group another_acl out
+ ip access-group test in
 !
-
 interface GigabitEthernet1/1/2
- description Another interface
- ip address 10.0.0.1 255.255.255.0
- ip access-group example_acl in
+ description Some description
+ ip address 192.168.1.1 255.255.255.0
+ ip access-group test_acl in
 !
 """
 
-interface_names_to_check = ["GigabitEthernet1/1/1", "GigabitEthernet1/1/2"]
-acl_names_to_check = ["test_acl", "another_acl", "example_acl"]
+interface_name_to_check = ["GigabitEthernet1/1/1","GigabitEthernet1/1/2"]
+acl_names_to_check = ["test_acl", "another_acl"]
 
-acl_information = get_interface_acls(interface_config_output, interface_names_to_check, acl_names_to_check)
-print(acl_information)
+info = get_interface_acls(interface_config_output, interface_name_to_check, acl_names_to_check)
+print(info)
