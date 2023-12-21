8ff51acc0fa566465648467b117d36066d815155
Sachin Verma <vsachin094@gmail.com>
2023-12-21 16:40:20 +0530

    21/12/2023

--- merge_csv.sh
+++ merge_csv.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-current_date=$(date +"%Y-%m-%d")
-oper_audit_directory="/path/to/oper_audit_splunk/"
-allchecks_directory="/path/to/allchecks_splunk/"
-output_directory="/path/to/output/directory/"
+# Function to find the latest file matching a pattern in a directory
+find_latest_matching_file() {
+    local directory="$1"
+    local pattern="$2"
 
-# Function to find the file for the current date
-find_file_for_date() {
-    local dir="$1"
-    local file_pattern="test_${current_date}_*_splunk.csv"
-    local matching_files=($dir$file_pattern)
+    # Find files matching the pattern in the directory
+    local matching_files=($(find "$directory" -type f -name "$pattern"))
 
-    if [ ${#matching_files[@]} -eq 1 ]; then
-        echo "${matching_files[0]}"
+    # Check if there are any matching files
+    if [ ${#matching_files[@]} -eq 0 ]; then
+        echo "No files matching the pattern found in $directory"
     else
-        echo "File for the current date not found or multiple matching files exist."
-        exit 1
+        # Get the latest file based on modification time
+        local latest_file=""
+        local latest_mtime=0
+        for file in "${matching_files[@]}"; do
+            local file_mtime=$(stat -c %Y "$file")
+            if [ $file_mtime -gt $latest_mtime ]; then
+                latest_mtime=$file_mtime
+                latest_file=$file
+            fi
+        done
+        echo "Latest file matching the pattern '$pattern':"
+        echo "$latest_file"
     fi
 }
 
-oper_audit_file=$(find_file_for_date "$oper_audit_directory")
-allchecks_file="${allchecks_directory}allchecks_splunk.csv"
-output_file="${output_directory}merged_data_${current_date}.csv"
+# Usage example: Call the function with directory and pattern
+find_latest_matching_file "/Users/sachin/Desktop/Projects_Personal/CLI2Structured" "test_*_*_test.csv"
 
-if [[ -f "$oper_audit_file" ]]; then
-    echo "Merging $oper_audit_file into $allchecks_file..."
-    paste -d ',' "$allchecks_file" "$oper_audit_file" >> "$output_file"
-    echo "Merge complete. Merged data saved to $output_file"
-else
-    echo "File for the current date ($current_date) not found in $oper_audit_directory."
-fi
